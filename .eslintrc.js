8c707334f7360ff36980d2ecb5f141145fece6e8
cmanor <101116379+cmanor@users.noreply.github.com>
2022-04-07 15:46:29 -0400

    linting complete

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,29 @@
 module.exports = {
-	"env": {
-		"browser": true,
-		"commonjs": true,
-		"es2021": true
+	'env': {
+		'browser': true,
+		'commonjs': true,
+		'es2021': true
 	},
-	"extends": "eslint:recommended",
-	"parserOptions": {
-		"ecmaVersion": "latest"
+	'extends': 'eslint:recommended',
+	'parserOptions': {
+		'ecmaVersion': 'latest'
 	},
-	"rules": {
-		"indent": [
-			"error",
-			"tab"
+	'rules': {
+		'indent': [
+			'error',
+			'tab'
 		],
-		"linebreak-style": [
-			"error",
-			"unix"
+		'linebreak-style': [
+			'error',
+			'unix'
 		],
-		"quotes": [
-			"error",
-			"single"
+		'quotes': [
+			'error',
+			'single'
 		],
-		"semi": [
-			"error",
-			"always"
+		'semi': [
+			'error',
+			'always'
 		],
 		'arrow-spacing': [
 			'error', { 'before': true, 'after': true }
--- index.js
+++ index.js
@@ -1,117 +1,117 @@
 //packages or middleware or some shit
-const express = require("express");
+const express = require('express');
 const app = express();
-const morgan = require("morgan");
-const cors = require('cors')
+const morgan = require('morgan');
+const cors = require('cors');
 
-app.use(cors())
-// app.use(morgan("tiny")); 
+app.use(cors());
+// app.use(morgan("tiny"));
 // can't use this and a custom token apparently
 app.use(express.json());
 ///have morgan return data in a post request
 morgan.token('DEEZ',  (req) => {
-if (req.method === 'POST')
-{
-return JSON.stringify(req.body);
-} else {
-  return null;
-}
+	if (req.method === 'POST')
+	{
+		return JSON.stringify(req.body);
+	} else {
+		return null;
+	}
 });
-app.use(morgan(":DEEZ"));
+app.use(morgan(':DEEZ'));
 
 //data
 let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Deez Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
+	{
+		id: 1,
+		name: 'Arto Hellas',
+		number: '040-123456',
+	},
+	{
+		id: 2,
+		name: 'Ada Lovelace',
+		number: '39-44-5323523',
+	},
+	{
+		id: 3,
+		name: 'Deez Abramov',
+		number: '12-43-234345',
+	},
+	{
+		id: 4,
+		name: 'Mary Poppendieck',
+		number: '39-23-6423122',
+	},
 ];
 //info page
 
-app.get("/info", (request, response) => {
-  const info = persons.length;
-  const now = new Date();
-  response.send(`Phonebook has info for  ${info} people <br>${now}`);
+app.get('/info', (request, response) => {
+	const info = persons.length;
+	const now = new Date();
+	response.send(`Phonebook has info for  ${info} people <br>${now}`);
 });
 
 // info for the perons page
-app.get("/api/persons", (request, response) => {
-  response.json(persons);
+app.get('/api/persons', (request, response) => {
+	response.json(persons);
 });
 //output a single entr
-app.get("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const singleE = persons.find((singleE) => singleE.id === id);
+app.get('/api/persons/:id', (request, response) => {
+	const id = Number(request.params.id);
+	const singleE = persons.find((singleE) => singleE.id === id);
 
-  if (singleE) {
-    response.json(singleE);
-  } else {
-    response.status(404).end();
-  }
+	if (singleE) {
+		response.json(singleE);
+	} else {
+		response.status(404).end();
+	}
 });
 //can delete things
-app.delete("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  persons = persons.filter((person) => person.id !== id);
-  response.status(204).end();
+app.delete('/api/persons/:id', (request, response) => {
+	const id = Number(request.params.id);
+	persons = persons.filter((person) => person.id !== id);
+	response.status(204).end();
 });
 //function for ID RNG
 const generateId = (IdMax) => Math.floor(Math.random() * IdMax);
 
 //posting stuff
-app.post("/api/persons", (request, response) => 
+app.post('/api/persons', (request, response) =>
 {
-  const body = request.body;
-  // error conditions for name
-  if (!body.name) {
-    return response.status(400).json({
-      error: "name missing",
-    });
-  }
-  //error conditions for number
-  if (!body.number) {
-    return response.status(400).json({
-      error: "number missing",
-    });
-  }
-  //posting parameters
-  const person = {
-    name: body.name,
-    number: body.number,
-    id: generateId(99999999999999),
-  };
-  //makes sure no duplicate names
-  for (i = 0; i < persons.length; i++) {
-    if (body.name == persons[i].name) {
-      return response.status(400).json({
-        error: "name must be unique",
-      });
-    }
-  }
-  //adds the new person to the persons array
-  persons = persons.concat(person)
-  //sends the person as the response from the server
-  response.json(person)
+	const body = request.body;
+	// error conditions for name
+	if (!body.name) {
+		return response.status(400).json({
+			error: 'name missing',
+		});
+	}
+	//error conditions for number
+	if (!body.number) {
+		return response.status(400).json({
+			error: 'number missing',
+		});
+	}
+	//posting parameters
+	const person = {
+		name: body.name,
+		number: body.number,
+		id: generateId(99999999999999),
+	};
+	//makes sure no duplicate names
+	for (let i = 0; i < persons.length; i++) {
+		if (body.name == persons[i].name) {
+			return response.status(400).json({
+				error: 'name must be unique',
+			});
+		}
+	}
+	//adds the new person to the persons array
+	persons = persons.concat(person);
+	//sends the person as the response from the server
+	response.json(person);
 });
 
 //sets up the port for the server
 const PORT = 3001;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+	console.log(`Server running on port ${PORT}`);
 });
